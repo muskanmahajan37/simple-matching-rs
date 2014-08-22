4e449fc29c9d42cd130f075dd2b30a5cb6e517b7
Eric <erjohnson.pdx@gmail.com>
2014-08-21 20:39:21 -0700

    Add pair to match

--- matching.rs
+++ matching.rs
@@ -1,3 +1,15 @@
 fn main() {
-  
+  let pair = (51i, true);
+
+  print!("Tell me about this tuple...");
+
+  match pair {
+    // if the bool is true and the int is between 20 and 26
+
+    // if the bool is true and the above isn't true for the int
+
+    // if bool is true or false and the int is between 40 and 49
+
+    // none of the previous conditions are true.
+  }
 }
