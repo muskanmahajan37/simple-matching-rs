c55c884d9dd864123628a12c93f9369ac24238ea
Eric <erjohnson.pdx@gmail.com>
2014-08-21 21:01:26 -0700

    Add test for true and not number range

--- test.rs
+++ test.rs
@@ -6,11 +6,21 @@ fn prints_pair() {
 }
 
 #[test]
-fn if_bool_true_int_20_to_26() {
+fn if_true_and_20_to_26() {
   let pair = (51i, true);
 
   match pair {
     (20..26, true) => { println!("True and between 20-26"); }
+    (_,_) => { println!("True or False and not between 20-26"); }
+  }
+}
+
+#[test]
+fn if_true_and_not_20_to_26() {
+  let pair = (51i, true);
+
+  match pair {
+    (x, true) if x != 20|21|22|23|24|25|26 => { println!("True and not between 20-26"); }
     (_,_) => { println!("True or False and not between 20-26"); }
   }
 }
